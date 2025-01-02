db635231cc2a2a7434f0e6037ed2b88bd6c79015
Zelal Hossain <zelal@ojjomedia.com>
2025-01-02 18:48:22 +0600

    cloudinary config enabled

--- apps/backend/config/plugins.ts
+++ apps/backend/config/plugins.ts
@@ -1,9 +1,24 @@
-export default () => ({
+export default ({ env }: { env: any }) => ({
   "padma-backend": {
     enabled: true,
   },
   "metajob-backend": {
     enabled: true,
     //resolve: "./src/plugins/metajob-backend",
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
   },
 });
