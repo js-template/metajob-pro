30de0a17537bc2139e167cd70c2d1f93df4ced97
Zelal Hossain <zelal@ojjomedia.com>
2025-02-18 19:00:53 +0600

    applied job widget added

---
+++ packages/metajob-theme/src/widgets/applied-job/index.tsx
@@ -0,0 +1,80 @@
+import { Grid } from "@mui/material"
+import { CountCard } from "../../shared/count-card"
+import CountCardLoader from "../../shared/count-card/loader"
+import _ from "lodash"
+
+import OpenError from "./error"
+import { IUserSession } from "../../types/user"
+import { find } from "@/lib/strapi"
+
+export const AppliedJob = async ({
+   block,
+   session
+}: {
+   block: any
+   session?: IUserSession | null | any
+   data?: any
+   language?: string
+}) => {
+   const { user } = session || {}
+   const { id: userId, role: userRole } = user || {}
+   const role = userRole?.type || ""
+
+   const styleData = block?.style || {}
+
+   // Fetch component data
+   const { data, error } = await find(
+      "api/padma-backend/private-frontpage",
+      {
+         populate: {
+            role1Components: {
+               on: {
+                  "widget.applied-list": {
+                     populate: "*"
+                  }
+               }
+            }
+         }
+      },
+      "no-store"
+   )
+
+   const { data: JobData, error: JobError } = await find(
+      "api/metajob-backend/applied-jobs",
+      {
+         fields: ["id"],
+         filters: {
+            owner: userId
+         }
+      },
+      "no-store"
+   )
+
+   const appliedJob = JobData?.meta?.pagination.total || 0
+
+   // Extract relevant data
+   const componentData =
+      data?.data?.role1Components?.find((comp: any) => comp.__component === "widget.applied-list")?.details || null
+
+   //console.log("Applied Job Data", componentData, "error", error)
+   const isLoading = !data && !error
+
+   return role === "candidate" ? (
+      <>
+         {isLoading ? (
+            <Grid item xs={styleData?.mobile} sm={styleData?.tab} md={styleData?.desktop}>
+               <CountCardLoader />
+            </Grid>
+         ) : (
+            <Grid item xs={styleData?.mobile} sm={styleData?.tab} md={styleData?.desktop}>
+               <CountCard item={componentData} count={appliedJob} />
+            </Grid>
+         )}
+      </>
+   ) : (
+      // Add message for candidate
+      <Grid item xs={styleData?.mobile} sm={styleData?.tab} md={styleData?.desktop}>
+         <OpenError />
+      </Grid>
+   )
+}
