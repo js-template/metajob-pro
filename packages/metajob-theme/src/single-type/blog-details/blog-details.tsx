354a7b9a2a65a41f401d6e3abf587531518fb758
mostafizur <86697071+mostafizur99@users.noreply.github.com>
2025-03-04 13:16:37 +0600

    blog-details page API server call updated

---
+++ packages/metajob-theme/src/single-type/blog-details/blog-details.tsx
@@ -0,0 +1,60 @@
+"use client"
+import { Container, Grid, Stack } from "@mui/material"
+import BlogContent from "./blog-content"
+import RecentPost from "./recent-post"
+import BlogCategory from "./blog-category"
+import BlogAds from "./blog-ads"
+import BlogHeader from "./header"
+import { IBlogCategory, IBlogDetailsBlock, ISinglePost } from "./types"
+
+type Props = {
+   block: IBlogDetailsBlock
+   data: ISinglePost
+   language?: string
+   recentBlogsData?: ISinglePost[]
+   categoryData?: IBlogCategory[]
+}
+
+const BlogDetailsServer = ({ data, block, recentBlogsData, categoryData }: Props) => {
+   const { title, sidebar } = block || {}
+   const isRightSidebar = !sidebar || sidebar === "Right Sidebar"
+   const isLeftSidebar = sidebar === "Left Sidebar"
+   const isBothSidebar = sidebar === "Both Sidebar"
+   const isNoSidebar = sidebar === "No Sidebar"
+
+   return (
+      <Stack>
+         <BlogHeader title={title || "Blog Details"} bg={data?.featuredImage} />
+         <Container maxWidth='lg' sx={{ py: 6 }}>
+            <Grid container spacing={4}>
+               {/* blog-details  */}
+               <Grid
+                  item
+                  xs={12}
+                  md={isBothSidebar ? 6 : isNoSidebar ? 12 : 8}
+                  order={{ xs: 1, md: isRightSidebar ? 1 : 2 }}>
+                  <BlogContent data={data} />
+               </Grid>
+               {!isNoSidebar && (
+                  <Grid item xs={12} md={isBothSidebar ? 3 : 4} order={{ xs: 2, md: isRightSidebar ? 2 : 1 }}>
+                     <Stack spacing={4}>
+                        <RecentPost recentBlogsData={recentBlogsData} />
+                        {!isBothSidebar && <BlogCategory blogCategoryData={categoryData} />}
+                        <BlogAds />
+                     </Stack>
+                  </Grid>
+               )}
+               {isBothSidebar && (
+                  <Grid item xs={12} md={isBothSidebar ? 3 : 4} order={{ xs: 3, md: isBothSidebar ? 3 : 3 }}>
+                     <Stack spacing={4}>
+                        <BlogCategory blogCategoryData={categoryData} />
+                     </Stack>
+                  </Grid>
+               )}
+            </Grid>
+         </Container>
+      </Stack>
+   )
+}
+
+export default BlogDetailsServer
