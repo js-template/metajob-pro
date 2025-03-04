6d1a41571e224f145171339817fcc00b010783a3
mostafizur <86697071+mostafizur99@users.noreply.github.com>
2025-03-04 16:28:17 +0600

    applied-job table API call updated

---
+++ packages/metajob-theme/src/blocks/applied-jobs/applied-jobs.tsx
@@ -0,0 +1,324 @@
+"use client"
+import { useEffect, useState } from "react"
+import { useSession } from "next-auth/react"
+import _ from "lodash"
+import { FormControl, Grid, MenuItem, Pagination, Paper, Select, TextField, useTheme } from "@mui/material"
+import Box from "@mui/material/Box"
+import Typography from "@mui/material/Typography"
+import { boxHeaderData } from "./data"
+import CIcon from "../../components/common/icon"
+import ListsTable from "./table"
+import { AccessError } from "../../shared/error-table"
+import { IAppliedJobsBlock, IApplyJobData } from "./type"
+import { find } from "../../lib/strapi"
+
+type Props = {
+   block: IAppliedJobsBlock
+   language?: string
+   appliedJobsPre: IApplyJobData[]
+}
+
+const AppliedJobsClient = ({ block, language, appliedJobsPre }: Props) => {
+   const theme = useTheme()
+
+   // session data destructuring
+   const { data: session } = useSession()
+   const { user } = session || {}
+   const { id: userId, role: userRole } = user || {}
+   const role = userRole?.type || ""
+
+   const { title, style, empty, table_config, table_head: tableHeader } = block || {}
+   const { label: tableLabel, enable_search: enableSearch, search_placeholder, default_data_count } = table_config || {}
+
+   //const [search, setSearch] = useState("")
+   const [pagination, setPagination] = useState<{
+      page: number
+      pageSize: number
+      pageCount: number
+      total: number
+   }>({
+      page: 1,
+      pageSize: 10,
+      pageCount: 1,
+      total: 0
+   })
+   const [applicationData, setApplicationData] = useState<IApplyJobData[]>(appliedJobsPre)
+   const [isLoading, setIsLoading] = useState(false)
+
+   //  fetch applied-job from db
+   useEffect(() => {
+      const getAppliedJobs = async () => {
+         setIsLoading(true)
+         const { data: appliedJobsDataAll, error: resumeError } = await find(
+            "api/metajob-backend/applied-jobs",
+            {
+               populate: "*",
+               pagination: {
+                  page: pagination.page,
+                  pageSize: pagination.pageSize,
+                  withCount: true
+               },
+               filters: {
+                  owner: {
+                     id: userId
+                  }
+                  // ...(search && {
+                  //    $or: [
+                  //       {
+                  //          title: { $containsi: search }
+                  //       },
+                  //       {
+                  //          slug: { $containsi: search }
+                  //       },
+                  //       {
+                  //          description: { $containsi: search }
+                  //       },
+                  //       {
+                  //          status: { $containsi: search }
+                  //       }
+                  //    ]
+                  // })
+               }
+            },
+            "no-store"
+         )
+         if (!resumeError) {
+            setApplicationData(appliedJobsDataAll?.data)
+            setPagination(appliedJobsDataAll?.meta?.pagination)
+            setIsLoading(false)
+         } else {
+            setApplicationData([])
+            setIsLoading(false)
+         }
+      }
+      if (userId) {
+         if (!userId) return
+         getAppliedJobs()
+      }
+
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [userId, pagination?.page])
+
+   return role === "candidate" ? (
+      <Grid item xs={12}>
+         <Paper
+            elevation={0}
+            sx={{
+               width: "100%",
+               border: "1px solid",
+               borderColor: "divider",
+               borderRadius: "12px",
+               p: 0,
+               mb: 5
+            }}>
+            <>
+               <Box
+                  sx={{
+                     px: 3,
+                     py: 2,
+                     borderBottom: "1px solid",
+                     borderColor: "divider",
+                     display: "flex",
+                     justifyContent: "space-between",
+                     alignItems: "center",
+                     gap: 3
+                  }}>
+                  <Typography
+                     variant='body1'
+                     fontWeight={700}
+                     fontSize={{
+                        xs: "1.25rem",
+                        sm: "1.5rem"
+                     }}
+                     lineHeight={"24px"}>
+                     {tableLabel}
+                  </Typography>
+               </Box>
+               {/* table data filter actions input fields */}
+               {/* {enableSearch && (
+                     <Box
+                        sx={{
+                           px: 3,
+                           py: 2,
+                           borderBottom: "1px solid",
+                           borderColor: "divider",
+                           display: "flex",
+                           gap: 3,
+                           alignItems: "center",
+                           flexWrap: "wrap"
+                        }}>
+                        
+                        <FormControl size='small'>
+                           <TextField
+                              id='outlined-basic'
+                              placeholder={boxHeaderData?.searchPlaceholder}
+                              variant='outlined'
+                              onChange={handleSearch}
+                              InputProps={{
+                                 endAdornment: (
+                                    <CIcon
+                                       icon='iconoir:search'
+                                       size={24}
+                                       sx={{
+                                          color: theme.palette.text.secondary
+                                       }}
+                                    />
+                                 )
+                              }}
+                              sx={{
+                                 "& .MuiInputBase-input": {
+                                    py: 1.2
+                                 },
+                                 "& .MuiInputBase-root": {
+                                    minHeight: "40px !important",
+                                    pl: theme.direction === "rtl" ? "10px !important" : "0px !important"
+                                 }
+                              }}
+                           />
+                        </FormControl>
+                     </Box>
+                  )} */}
+
+               {/* Table */}
+               <ListsTable
+                  headCells={tableHeader}
+                  data={applicationData}
+                  isLoading={isLoading}
+                  empty={empty}
+                  pageSize={pagination.pageSize}
+               />
+
+               {/* Box Footer */}
+               <Box
+                  sx={{
+                     py: 2.5,
+                     px: 3,
+                     borderTop: "1px solid",
+                     borderColor: "divider",
+                     display: "flex",
+                     justifyContent: "space-between",
+                     alignItems: "center"
+                  }}>
+                  <Box
+                     sx={{
+                        display: "flex",
+                        gap: 1,
+                        alignItems: "center"
+                     }}>
+                     {/* <Typography variant='body1' fontWeight={500} lineHeight={"24px"}>
+                        Per page
+                     </Typography> */}
+                     <FormControl size='small'>
+                        <Select
+                           labelId='per_page'
+                           id='per_page'
+                           autoWidth
+                           defaultValue={default_data_count || 10}
+                           onChange={(e) => {
+                              setPagination({ ...pagination, pageSize: e.target.value as number })
+                           }}
+                           renderValue={(selected) => {
+                              const selectedValue = selected as number
+
+                              return (
+                                 <Typography variant='body1' fontWeight={500} lineHeight={"24px"}>
+                                    {selectedValue}
+                                 </Typography>
+                              )
+                           }}
+                           IconComponent={() => (
+                              <CIcon
+                                 icon='iconamoon:arrow-down-2-duotone'
+                                 size={20}
+                                 sx={{
+                                    position: "absolute",
+                                    right: "8px",
+                                    top: "50%",
+                                    transform: "translateY(-50%)",
+                                    pointerEvents: "none"
+                                 }}
+                              />
+                           )}
+                           sx={{
+                              backgroundColor: (theme) => theme.palette.text.primary + "10",
+                              borderRadius: "8px",
+                              borderColor: "divider",
+                              minWidth: "70px",
+                              "& .MuiSelect-select": {
+                                 px: 1.8,
+                                 py: 1
+                              }
+                           }}>
+                           {_.map(boxHeaderData?.showingPerPage?.options, (option, index) => (
+                              <MenuItem key={index} value={option}>
+                                 {option}
+                              </MenuItem>
+                           ))}
+                        </Select>
+                     </FormControl>
+                  </Box>
+                  <Box>
+                     <Pagination
+                        count={pagination.pageCount}
+                        variant='text'
+                        shape='rounded'
+                        color='primary'
+                        size='large'
+                        siblingCount={0}
+                        onChange={(event, page) => {
+                           setPagination({ ...pagination, page })
+                        }}
+                        dir={theme.direction}
+                        sx={{
+                           "& li": {
+                              borderRadius: 0,
+                              height: "40px",
+                              margin: 0
+                           },
+                           "& .MuiButtonBase-root": {
+                              margin: 0,
+                              border: "none",
+                              borderLeft: "1px solid",
+                              borderTop: "1px solid",
+                              borderBottom: "1px solid",
+                              borderColor: "divider",
+                              borderRadius: 0,
+                              "&:hover": {
+                                 backgroundColor: (theme) => theme.palette.action.hover
+                              }
+                           },
+                           "& li:last-child .MuiButtonBase-root": {
+                              borderRadius: theme.direction === "rtl" ? "6px 0px 0px 6px" : "0px 6px 6px 0px",
+                              borderRight: "1px solid",
+                              borderColor: "divider"
+                           },
+                           "& li:first-child .MuiButtonBase-root": {
+                              borderRadius: theme.direction === "rtl" ? "0px 6px 6px 0px" : "6px 0px 0px 6px",
+                              borderLeft: "1px solid",
+                              borderColor: "divider"
+                           },
+                           "& .MuiPaginationItem-ellipsis": {
+                              borderTop: "1px solid",
+                              borderBottom: "1px solid",
+                              borderLeft: "1px solid",
+                              borderColor: "divider",
+                              height: "100%",
+                              margin: 0,
+                              borderRadius: 0,
+                              display: "flex",
+                              justifyContent: "center",
+                              alignItems: "center"
+                           }
+                        }}
+                     />
+                  </Box>
+               </Box>
+            </>
+         </Paper>
+      </Grid>
+   ) : (
+      <AccessError roleValue={"Candidate"} />
+   )
+}
+
+export default AppliedJobsClient
--- packages/metajob-theme/src/blocks/applied-jobs/index.tsx
+++ packages/metajob-theme/src/blocks/applied-jobs/index.tsx
@@ -1,15 +1,7 @@
-"use client"
-import { useState } from "react"
-import { useSession } from "next-auth/react"
-import _ from "lodash"
-import useSWR from "swr"
-import { FormControl, Grid, MenuItem, Pagination, Paper, Select, TextField, useTheme } from "@mui/material"
-import Box from "@mui/material/Box"
-import Typography from "@mui/material/Typography"
-import { boxHeaderData } from "./data"
-import CIcon from "../../components/common/icon"
-import ListsTable from "./table"
-import { AccessError } from "../../shared/error-table"
+"use server"
+import { auth } from "../../lib/auth"
+import { find } from "../../lib/strapi"
+import AppliedJobsClient from "./applied-jobs"
 import { IAppliedJobsBlock } from "./type"
 
 type Props = {
@@ -17,302 +9,31 @@ type Props = {
    language?: string
 }
 
-export const AppliedJobs = ({ block, language }: Props) => {
-   const theme = useTheme()
+export const AppliedJobs = async ({ block, language }: Props) => {
+   // Get user session
+   const session = await auth()
 
-   // session data destructuring
-   const { data: session } = useSession()
-   const { user } = session || {}
-   const { id: userId, role: userRole } = user || {}
-   const role = userRole?.type || ""
+   // get user data
+   const userId = session?.user?.id
+   const { data: appliedJobsDataAll } = userId
+      ? await find(
+           "api/metajob-backend/applied-jobs",
+           {
+              populate: "*",
+              pagination: {
+                 page: 1,
+                 pageSize: 10,
+                 withCount: true
+              },
+              filters: {
+                 owner: {
+                    id: userId
+                 }
+              }
+           },
+           "no-store"
+        )
+      : {}
 
-   //const [search, setSearch] = useState("")
-   const [pagination, setPagination] = useState<{
-      page: number
-      pageSize: number
-      pageCount: number
-      total: number
-   }>({
-      page: 1,
-      pageSize: 10,
-      pageCount: 1,
-      total: 0
-   })
-
-   const { title, style, empty, table_config, table_head: tableHeader } = block || {}
-   const { label: tableLabel, enable_search: enableSearch, search_placeholder, default_data_count } = table_config || {}
-
-   // *** fetcher function for SWR
-   const fetcher = async (url: string) => {
-      const response = await fetch(url)
-      if (!response.ok) {
-         throw new Error("An error occurred while fetching the data.")
-      }
-      const result = await response.json()
-      setPagination(result?.data?.meta?.pagination)
-      return result?.data // Return the nested data
-   }
-
-   // Fetcher function for SWR
-   const queryParams = {
-      // populate: "deep",
-      populate: "*",
-      pagination: {
-         page: pagination.page,
-         pageSize: pagination.pageSize,
-         withCount: true
-      },
-      filters: {
-         owner: {
-            id: userId
-         }
-         // ...(search && {
-         //    $or: [
-         //       {
-         //          title: { $containsi: search }
-         //       },
-         //       {
-         //          slug: { $containsi: search }
-         //       },
-         //       {
-         //          description: { $containsi: search }
-         //       },
-         //       {
-         //          status: { $containsi: search }
-         //       }
-         //    ]
-         // })
-      }
-   }
-
-   // Convert queryParams to a string for the URL
-   const queryString = encodeURIComponent(JSON.stringify(queryParams))
-
-   // Construct the API URL
-   const apiUrl = `/api/find?model=api/metajob-backend/applied-jobs&query=${queryString}&cache=no-store`
-
-   const { data: listsData, error, isLoading, mutate } = useSWR(apiUrl, fetcher)
-
-   const applicationData = listsData?.data || []
-
-   return role === "candidate" ? (
-      <Grid item xs={12}>
-         <Paper
-            elevation={0}
-            sx={{
-               width: "100%",
-               border: "1px solid",
-               borderColor: "divider",
-               borderRadius: "12px",
-               p: 0,
-               mb: 5
-            }}>
-            <>
-               <Box
-                  sx={{
-                     px: 3,
-                     py: 2,
-                     borderBottom: "1px solid",
-                     borderColor: "divider",
-                     display: "flex",
-                     justifyContent: "space-between",
-                     alignItems: "center",
-                     gap: 3
-                  }}>
-                  <Typography
-                     variant='body1'
-                     fontWeight={700}
-                     fontSize={{
-                        xs: "1.25rem",
-                        sm: "1.5rem"
-                     }}
-                     lineHeight={"24px"}>
-                     {tableLabel}
-                  </Typography>
-               </Box>
-               {/* table data filter actions input fields */}
-               {/* {enableSearch && (
-                     <Box
-                        sx={{
-                           px: 3,
-                           py: 2,
-                           borderBottom: "1px solid",
-                           borderColor: "divider",
-                           display: "flex",
-                           gap: 3,
-                           alignItems: "center",
-                           flexWrap: "wrap"
-                        }}>
-                        
-                        <FormControl size='small'>
-                           <TextField
-                              id='outlined-basic'
-                              placeholder={boxHeaderData?.searchPlaceholder}
-                              variant='outlined'
-                              onChange={handleSearch}
-                              InputProps={{
-                                 endAdornment: (
-                                    <CIcon
-                                       icon='iconoir:search'
-                                       size={24}
-                                       sx={{
-                                          color: theme.palette.text.secondary
-                                       }}
-                                    />
-                                 )
-                              }}
-                              sx={{
-                                 "& .MuiInputBase-input": {
-                                    py: 1.2
-                                 },
-                                 "& .MuiInputBase-root": {
-                                    minHeight: "40px !important",
-                                    pl: theme.direction === "rtl" ? "10px !important" : "0px !important"
-                                 }
-                              }}
-                           />
-                        </FormControl>
-                     </Box>
-                  )} */}
-
-               {/* Table */}
-               <ListsTable
-                  headCells={tableHeader}
-                  data={applicationData}
-                  isLoading={isLoading}
-                  empty={empty}
-                  pageSize={pagination.pageSize}
-               />
-
-               {/* Box Footer */}
-               <Box
-                  sx={{
-                     py: 2.5,
-                     px: 3,
-                     borderTop: "1px solid",
-                     borderColor: "divider",
-                     display: "flex",
-                     justifyContent: "space-between",
-                     alignItems: "center"
-                  }}>
-                  <Box
-                     sx={{
-                        display: "flex",
-                        gap: 1,
-                        alignItems: "center"
-                     }}>
-                     {/* <Typography variant='body1' fontWeight={500} lineHeight={"24px"}>
-                        Per page
-                     </Typography> */}
-                     <FormControl size='small'>
-                        <Select
-                           labelId='per_page'
-                           id='per_page'
-                           autoWidth
-                           defaultValue={default_data_count || 10}
-                           onChange={(e) => {
-                              setPagination({ ...pagination, pageSize: e.target.value as number })
-                           }}
-                           renderValue={(selected) => {
-                              const selectedValue = selected as number
-
-                              return (
-                                 <Typography variant='body1' fontWeight={500} lineHeight={"24px"}>
-                                    {selectedValue}
-                                 </Typography>
-                              )
-                           }}
-                           IconComponent={() => (
-                              <CIcon
-                                 icon='iconamoon:arrow-down-2-duotone'
-                                 size={20}
-                                 sx={{
-                                    position: "absolute",
-                                    right: "8px",
-                                    top: "50%",
-                                    transform: "translateY(-50%)",
-                                    pointerEvents: "none"
-                                 }}
-                              />
-                           )}
-                           sx={{
-                              backgroundColor: (theme) => theme.palette.text.primary + "10",
-                              borderRadius: "8px",
-                              borderColor: "divider",
-                              minWidth: "70px",
-                              "& .MuiSelect-select": {
-                                 px: 1.8,
-                                 py: 1
-                              }
-                           }}>
-                           {_.map(boxHeaderData?.showingPerPage?.options, (option, index) => (
-                              <MenuItem key={index} value={option}>
-                                 {option}
-                              </MenuItem>
-                           ))}
-                        </Select>
-                     </FormControl>
-                  </Box>
-                  <Box>
-                     <Pagination
-                        count={pagination.pageCount}
-                        variant='text'
-                        shape='rounded'
-                        color='primary'
-                        size='large'
-                        siblingCount={0}
-                        onChange={(event, page) => {
-                           setPagination({ ...pagination, page })
-                        }}
-                        dir={theme.direction}
-                        sx={{
-                           "& li": {
-                              borderRadius: 0,
-                              height: "40px",
-                              margin: 0
-                           },
-                           "& .MuiButtonBase-root": {
-                              margin: 0,
-                              border: "none",
-                              borderLeft: "1px solid",
-                              borderTop: "1px solid",
-                              borderBottom: "1px solid",
-                              borderColor: "divider",
-                              borderRadius: 0,
-                              "&:hover": {
-                                 backgroundColor: (theme) => theme.palette.action.hover
-                              }
-                           },
-                           "& li:last-child .MuiButtonBase-root": {
-                              borderRadius: theme.direction === "rtl" ? "6px 0px 0px 6px" : "0px 6px 6px 0px",
-                              borderRight: "1px solid",
-                              borderColor: "divider"
-                           },
-                           "& li:first-child .MuiButtonBase-root": {
-                              borderRadius: theme.direction === "rtl" ? "0px 6px 6px 0px" : "6px 0px 0px 6px",
-                              borderLeft: "1px solid",
-                              borderColor: "divider"
-                           },
-                           "& .MuiPaginationItem-ellipsis": {
-                              borderTop: "1px solid",
-                              borderBottom: "1px solid",
-                              borderLeft: "1px solid",
-                              borderColor: "divider",
-                              height: "100%",
-                              margin: 0,
-                              borderRadius: 0,
-                              display: "flex",
-                              justifyContent: "center",
-                              alignItems: "center"
-                           }
-                        }}
-                     />
-                  </Box>
-               </Box>
-            </>
-         </Paper>
-      </Grid>
-   ) : (
-      <AccessError roleValue={"Candidate"} />
-   )
+   return <AppliedJobsClient block={block} language={language} appliedJobsPre={appliedJobsDataAll?.data || []} />
 }
