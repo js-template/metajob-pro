ac10ccec86fdeb55cc65964704a1f71c9edc9b07
mostafizur <86697071+mostafizur99@users.noreply.github.com>
2025-03-12 23:03:34 +0600

    home: category-card title issue fixed

--- packages/metajob-theme/src/blocks/job-category/item.tsx
+++ packages/metajob-theme/src/blocks/job-category/item.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image"
 import NextLink from "next/link"
 import { Stack, Box, Typography, Card } from "@mui/material"
-import { hexToRGBA } from "../../lib/hex-to-rgba"
 import { ISingleCategory } from "./types"
 
 export const CardItem = ({ data }: { data: ISingleCategory }) => {
@@ -25,23 +24,11 @@ export const CardItem = ({ data }: { data: ISingleCategory }) => {
             }
          }}>
          <Stack
-            spacing={2}
             sx={{
                justifyContent: "center",
-               alignItems: "center"
+               alignItems: "center",
+               mb: 2
             }}>
-            {/* <Box
-               className='iconBox'
-               sx={{
-                  color: (theme) => theme.palette.primary.main,
-                  bgcolor: (theme) => hexToRGBA(theme.palette.primary.main, 0.1),
-                  borderRadius: "12px",
-                  width: "fit-content",
-                  p: 2,
-                  transition: "transform 0.3s ease-in-out"
-               }}>
-               <Image src={logo} alt='icon' height={20} width={20} />
-            </Box> */}
             <Box
                className='iconBox'
                sx={{
@@ -51,26 +38,25 @@ export const CardItem = ({ data }: { data: ISingleCategory }) => {
                }}>
                {logo && <Image src={logo || "https://placehold.co/60/png"} alt='icon' height={60} width={60} />}
             </Box>
-
-            <Stack>
-               <Typography
-                  sx={{
-                     color: (theme) => theme.palette.text.primary,
-                   }}
-                  fontWeight={400}
-                  fontSize={16}
-                  textAlign={"center"}
-                  noWrap>
-                  {title}
-               </Typography>
-               {/* <Typography
+         </Stack>
+         <Stack>
+            <Typography
+               sx={{
+                  color: (theme) => theme.palette.text.primary
+               }}
+               fontWeight={400}
+               fontSize={16}
+               textAlign={"center"}
+               noWrap>
+               {title}
+            </Typography>
+            {/* <Typography
                    color={(theme) => theme.palette.text.secondary}
                    fontWeight={400}
                    fontSize={14}
                    textAlign={"center"}>
                    99 Jobs
                 </Typography> */}
-            </Stack>
          </Stack>
       </Card>
    )
