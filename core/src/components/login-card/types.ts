ffa92703d398555d22b51b1bc254f84fb9f0074d
mostafizur <mostafizur.mr33@gmail.com>
2025-04-13 11:45:45 +0600

    login page customization added

--- core/src/components/login-card/types.ts
+++ core/src/components/login-card/types.ts
@@ -8,4 +8,17 @@ export type ILoginBLock = {
    signup_link_placeholder?: string
    provider_option?: boolean
    signup_helper_placeholder?: string
+   styles?: {
+      color?: any
+      secondary_color?: string
+      backgroundColor?: any
+      mobile: number
+      tab: number
+      desktop: number
+      bg_overlay?: number
+      header_color?: string
+      sub_header_color: string
+      header_width: "Full" | "Small"
+      section_padding?: number
+   }
 } | null
