bb32188e8020700bf7855c1ac2f5a6753b779f4f
mostafizur <mostafizur.mr33@gmail.com>
2025-04-14 15:22:11 +0600

    create job date, vacancy, skills field updated

--- packages/metajob-theme/src/blocks/manage-job/hook.ts
+++ packages/metajob-theme/src/blocks/manage-job/hook.ts
@@ -32,3 +32,30 @@ export const getFeaturedCount = (data: IJobData[]) => {
    const featuredJobs = data.filter((item) => item?.is_featured)
    return featuredJobs?.length || 0
 }
+
+/**
+ * Get the slug from the title.
+ * @param {string} title - The title to be converted to a slug.
+ * @returns {string} - The generated slug.
+ */
+export const getSlugFromTitle = (title?: string) => {
+   if (!title) {
+      return ""
+   }
+   const newSlug = title
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, "") // remove non-word chars
+      .replace(/[\s_-]+/g, "-") // collapse whitespace and underscores
+      .replace(/^-+|-+$/g, "") // trim leading/trailing -
+   return newSlug
+}
+
+/**
+ * Format a date to YYYY-MM-DD.
+ * @param {Date} date - The date to be formatted.
+ * @returns {string} - The formatted date string.
+ * */
+export const formatDate = (date: Date) => {
+   return date.toISOString().split("T")[0] // Format to YYYY-MM-DD
+}
